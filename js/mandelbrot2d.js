365546373be61ef24f57ed62c057f02d24eab8d0
avery <asommer999@gmail.com>
2021-05-31 22:41:17 -0400

    added comments

--- js/mandelbrot2d.js
+++ js/mandelbrot2d.js
@@ -1,22 +1,30 @@
 var canvas = document.getElementById('myCanvas')
 var ctx = canvas.getContext('2d')
 
+// set canvas to window resolution
 const WIDTH = window.innerWidth
 const HEIGHT = window.innerHeight
 ctx.canvas.width = WIDTH
 ctx.canvas.height = HEIGHT
 
+// axis
 const REAL_SET = { start: -2, end: 1 }
 const IMAGINARY_SET = { start: -1, end: 1 }
 
+// generate 24 random colors
 const colors = new Array(16).fill(0).map((_, i) => i === 0 ? '#000' : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
 
 function draw() {
+    // iterate through each pixel
     for (let i = 0; i < WIDTH; i++) {
         for (let j = 0; j < HEIGHT; j++) {
+
+            // generate the complex number
             complex = {
-                x: REAL_SET.start + (i / WIDTH) * (REAL_SET.end - REAL_SET.start),
-                y: IMAGINARY_SET.start + (j / HEIGHT) * (IMAGINARY_SET.end - IMAGINARY_SET.start)
+
+                // mx + b
+                r: REAL_SET.start + (i / WIDTH) * (REAL_SET.end - REAL_SET.start),
+                i: IMAGINARY_SET.start + (j / HEIGHT) * (IMAGINARY_SET.end - IMAGINARY_SET.start)
             }
 
             const [m, isMandelbrotSet] = mandelbrot(complex)
@@ -27,17 +35,21 @@ function draw() {
 }
 const MAX_ITERATION = 80
 function mandelbrot(c) {
-    let z = { x: 0, y: 0 }, n = 0, p, d;
+    // starting at z=0
+    let z = { r: 0, i: 0 }, n = 0, p, d;
     do {
+        // do Z^2
         p = {
-            x: Math.pow(z.x, 2) - Math.pow(z.y, 2),
-            y: 2 * z.x * z.y
+            r: Math.pow(z.r, 2) - Math.pow(z.i, 2),
+            i: 2 * z.r * z.i
         }
+        // add c
         z = {
-            x: p.x + c.x,
-            y: p.y + c.y
+            r: p.r + c.r,
+            i: p.i + c.i
         }
-        d = Math.sqrt(Math.pow(z.x, 2) + Math.pow(z.y, 2))
+        // |z|
+        d = Math.sqrt(Math.pow(z.r, 2) + Math.pow(z.i, 2))
         n += 1
     } while (d <= 2 && n < MAX_ITERATION)
     return [n, d <= 2]
