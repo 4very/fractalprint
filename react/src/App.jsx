6fc33cbd4cf999a9693b22fda6475c7a2b15fc87
avery <asommer999@gmail.com>
2021-06-04 22:34:47 -0400

    house keeping 🏠

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { Shaders, Node, GLSL, Uniform } from "gl-react";
-import { Surface } from "gl-react-dom";
-import shader from "../shaders/mandelbrot2d.glsl?raw";
+import { Shaders, Node, GLSL, Uniform } from 'gl-react';
+import { Surface } from 'gl-react-dom';
+import shader from '../shaders/mandelbrot2d.glsl?raw';
 
 const shaders = Shaders.create({
   helloGL: {
@@ -9,16 +8,14 @@ const shaders = Shaders.create({
   },
 });
 
-const WIDTH = 1000
-const HEIGHT = 1000
-const size = [WIDTH,HEIGHT]
+const WIDTH = 1000;
+const HEIGHT = 1000;
+const size = [WIDTH, HEIGHT];
 
 function App() {
-
-
   return (
     <Surface width={WIDTH} height={HEIGHT}>
-      <Node shader={shaders.helloGL} uniforms={{size}}/>
+      <Node shader={shaders.helloGL} uniforms={{ size }} />
     </Surface>
   );
 }
